81b8ad820f9e138ed25de3c196e75c1e996e1e04
user <user@DESKTOP-N9O301I>
2020-06-05 15:28:40 +0900

    06.05.WindowBuilderTest

---
+++ KoseaMiddleProject/src/loginPack/LoginStart.java
@@ -0,0 +1,40 @@
+package loginPack;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class LoginStart extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		try {
+			LoginStart frame = new LoginStart();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginStart() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
