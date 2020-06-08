0dd292c01dfc61faf36b6d4758d5b0fc871daee2
user <user@DESKTOP-N9O301I>
2020-06-08 12:52:43 +0900

    06.08.add connDB

---
+++ KoseaMiddleProject/src/connPack/ConnDB.java
@@ -0,0 +1,29 @@
+package connPack;
+
+import java.sql.*;
+
+public class ConnDB {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	String user = "hrchin";
+	String password = "kosea";
+	
+	private Connection con;
+	private Statement stmt;
+	private ResultSet rs;
+	
+	public void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("jdbc driver loading success");
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("oracle connetion success");
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			System.out.println("statement create success." +"\n\n");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
